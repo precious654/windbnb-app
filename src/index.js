4a21fb9e099be9db0e3b532c17523f3846bb7a5c
Odukwe Precious <odukwechiemeka@gmail.com>
2022-09-04 03:04:18 +0100

    almost there

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import "./style.min.css";
+import "./Styles/style.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render( <App /> );
